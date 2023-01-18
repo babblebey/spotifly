f8bc105d4721c65044079c56cb86071cf46a2a98
babblebey <babblebey@gmail.com>
2023-01-18 20:46:49 +0100

    Boilerplated NextJS x Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Spotify</title>
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Spotify Clone
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Getting started
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
