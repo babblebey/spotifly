2614e9a2594504f5d408eb576282a7126a8a4503
babblebey <babblebey@gmail.com>
2023-01-22 19:17:01 +0100

    Added 'active' prop to icon component interface with updated updated svg path

--- icons/LibraryIcon.tsx
+++ icons/LibraryIcon.tsx
@@ -4,14 +4,19 @@ interface Props {
     height?: number,
     width?: number,
     color?: string,
-    className?: string
+    className?: string,
+    active?: boolean
 }
  
-const LibraryIcon: FC<Props> = ({ height, width, color, className }) => {
+const LibraryIcon: FC<Props> = ({ height, width, color, className, active }) => {
     return ( 
         <>
             <svg role="img" fill={color} height={height} width={width} aria-hidden="true" className={className} viewBox="0 0 24 24" data-encore-id="icon">
-                <path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"></path>
+                { !active ? (
+                    <path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"></path>
+                ) : (
+                    <path d="M3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zM15.5 2.134A1 1 0 0014 3v18a1 1 0 001 1h6a1 1 0 001-1V6.464a1 1 0 00-.5-.866l-6-3.464zM9 2a1 1 0 00-1 1v18a1 1 0 102 0V3a1 1 0 00-1-1z"></path>
+                ) }
             </svg>
         </>
     );
