fe7b5af248222e112a9de969221ad475bf0f2284
babblebey <babblebey@gmail.com>
2023-01-23 17:01:34 +0100

    Boilerplated Player component

---
+++ components/Player.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface PlayerProps {
+    
+}
+ 
+const Player: FC<PlayerProps> = () => {
+    return ( 
+        <div className="">
+            Player
+        </div>
+     );
+}
+ 
+export default Player;
