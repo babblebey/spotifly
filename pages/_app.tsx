cf05aeb648547c00e793dff68a15e2fc23dc783c
babblebey <babblebey@gmail.com>
2023-01-23 17:00:49 +0100

    Set a new _app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { circular } from "../fonts"
+import Navbar from "../components/Navbar"
+import Player from "../components/Player"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={`${circular.variable} font-sans`}>
-      <Component {...pageProps} />
+      <div className="sm:flex w-full sm:h-screen sm:overflow-hidden">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Pages Content */}
+        <div className="page">
+          <Component {...pageProps} />
+        </div>
+
+        {/* Player */}
+        <Player />
+      </div>
     </main>
   )
 }
