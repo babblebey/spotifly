eccf5fe2cf5925f1e8ad30e37acae3509db18443
babblebey <babblebey@gmail.com>
2023-01-26 20:16:07 +0100

    Improved HTML syntax and summarised utility classes in component

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -8,7 +8,7 @@ interface PageHeaderProps {
 
 const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
     return ( 
-        <div className="flex items-center space-x-4 py-4 px-4 sm:px-8 h-16 w-full sticky top-0 left-0 bg-sdark-base bg-opacity-50">
+        <header className="flex items-center space-x-4 py-4 px-4 sm:px-8 h-16 w-full sticky top-0 left-0 bg-sdark-base">
             {/* Navigation */}
             <div className={`${(!isLoggedIn || variant === 'home') && 'grow'} flex space-x-4`}>
                 <button className="flex h-8 w-8 items-center justify-center bg-black rounded-full">
@@ -22,7 +22,7 @@ const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
             {/* Playlist - Play Button/Title  */}
             {isLoggedIn && variant === 'playlist' && (
                 <div className={`${true && 'grow'} text-white flex items-center space-x-4`}>
-                    <button className="bg-sgreen-100 h-12 w-12 rounded-full flex items-center justify-center hover:scale-105">
+                    <button className="play_button h-12 w-12">
                         <PlayIcon width={28} height={24} />
                     </button>
                     <h2 className="text-2xl font-extrabold">
@@ -86,7 +86,7 @@ const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
             {isLoggedIn ? (
                 <div>
                     <button className="flex items-center space-x-2 text-white text-sm font-bold rounded-full bg-black p-[2px] md:pr-2">
-                        <div className="flex items-center h-7 w-7 justify-center rounded-full bg-[#535353]">
+                        <div className="flex items-center h-7 w-7 justify-center rounded-full bg-sdark-53">
                             <UserIcon width={16} height={16} />
                         </div>
                         <p className="hidden md:block">
@@ -97,7 +97,7 @@ const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
                 </div>
             ) : (
                 <div className="flex items-center space-x-6">
-                    <button className="text-white font-bold opacity-80 hover:opacity-100 hover:scale-105">
+                    <button className="text-swhite-subdued font-bold hover:text-white hover:scale-105">
                         <p>Sign up</p>
                     </button>
                     <button className="h-12 w-28 rounded-full bg-white text-black font-bold hover:scale-105 hover:bg-slate-50">
@@ -105,7 +105,7 @@ const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
                     </button>
                 </div>
             )}
-        </div>
+        </header>
     );
 }
  
