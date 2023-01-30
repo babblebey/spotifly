8fcde23014fbb2ce2b792451473cbc466c4cd9ca
babblebey <babblebey@gmail.com>
2023-01-30 15:29:32 +0100

    Updated card style

--- components/ArtistCard.tsx
+++ components/ArtistCard.tsx
@@ -8,9 +8,11 @@ interface ArtistCardProps {
  
 const ArtistCard: FC<ArtistCardProps> = () => {
     return ( 
-        <div className="max-w-[200px] pt-3 pb-6 px-4 rounded space-y-3 group bg-sdark-el-base hover:bg-sdark-el-base-highlight">
+        <div className="artist_card group section_item">
             <div className="relative">
-                <Image src={'/a1.jfif'} width={200} height={200} alt="title" className="rounded-full shadow-2xl shadow-black" />
+                <Image src={'/a1.jfif'} width={200} height={200} alt="title" 
+                    className="rounded-full shadow-2xl shadow-black w-full" 
+                />
                 <button className="play_button_hide_show h-12 w-12 shadow-lg shadow-sdark-el-base">
                     <PlayIcon width={24} height={24} />
                 </button>
