5b6da48554e4503cc44ecff80e453b3caffa9f0f
babblebey <babblebey@gmail.com>
2023-01-31 14:53:20 +0100

    Added albums page

---
+++ pages/collection/albums.tsx
@@ -0,0 +1,43 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import PageFooter from "../../components/PageFooter";
+import PageHeader from "../../components/PageHeader";
+import PlaylistCard from "../../components/PlaylistCard";
+
+interface AlbumsProps {
+    items: object[]
+}
+
+const Albums: NextPage<AlbumsProps> = ({ items }) => {
+    const list = [1,2,3,4,5,6,7,8,9];
+
+    return (
+        <div className="relative">
+            <Head>
+                <title>Spotifly - Your Library</title>
+            </Head>
+
+            <PageHeader variant="library" isLoggedIn />
+
+            <main className="content">
+                <div className="space-y-5 section @container/section">
+                    <div className="flex items-center">
+                        <p className="_title">
+                            Albums
+                        </p>
+                    </div>
+
+                    <div className="items @container-normal gap-y-6">
+                        { list.map((i, l) => (
+                            <PlaylistCard key={i} />
+                        )) }
+                    </div>
+                </div>
+            </main>
+
+            <PageFooter isLoggedIn />
+        </div>    
+    )
+}   
+
+export default Albums
