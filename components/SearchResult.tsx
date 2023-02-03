6cfd777a193e7713ee9ec6918a3ae6266db12cab
babblebey <babblebey@gmail.com>
2023-02-03 11:27:52 +0100

    Added SearchResult component

---
+++ components/SearchResult.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { FC } from "react";
+import { PlayIcon } from "../icons";
+import PlaylistSection from "./PlaylistSection";
+import SongListItem from "./SongListItem";
+
+interface SearchResultProps {
+    
+}
+ 
+const SearchResult: FC<SearchResultProps> = () => {
+    return ( 
+        <div className="space-y-10">
+            {/* Top Section */}
+            <div className="section @container/section">
+                <div className="grid grid-cols-2 gap-y-6 search_result_top @container/search-top">
+                    {/* Top Result */}
+                    <div className="col-span-2 space-y-5">
+                        <p className="_title">
+                            Top Result
+                        </p>
+                        <div className="relative group bg-sdark-el-base hover:bg-sdark-el-base-highlight p-5 rounded text-white flex flex-col select-none">
+                            <div className="h-24 w-24">
+                                <Image src={'/ar1.jfif'} width={200} height={200} alt="title" className="rounded-full w-full" />
+                            </div>
+                            <div className="flex items-center mt-6">
+                                <p className="font-black text-3xl leading-none">
+                                    Title
+                                </p>
+                            </div>
+                            <div className="flex items-center space-x-2 mt-3 text-sm">
+                                {false && <p className="text-swhite-subdued">
+                                    Name
+                                </p>}
+                                <button className="bg-sdark-base font-bold uppercase rounded-full py-1 px-3">
+                                    Artist
+                                </button>
+                            </div>
+                            <button className="play_button_hide_show h-12 w-12 m-4 shadow-lg shadow-sdark-base">
+                                <PlayIcon width={24} height={24} />
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Songs */}
+                    <div className="songs_search_result space-y-5">
+                        <p className="_title">
+                            Songs
+                        </p>
+
+                        <div>
+                            <SongListItem />
+                            <SongListItem />
+                            <SongListItem />
+                            <SongListItem />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <PlaylistSection title="Featuring Wizkid" hideOverflow />
+            <PlaylistSection title="Featuring Wizkid" hideOverflow />
+        </div>
+     );
+}
+ 
+export default SearchResult;
