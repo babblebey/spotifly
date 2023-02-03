68cccfd7a595318e7a5e6fd3fc804a3138e4b03e
babblebey <babblebey@gmail.com>
2023-02-03 11:27:25 +0100

    Added container queries to elements

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { ChevronLeftIcon, ChevronRightIcon, PlayIcon, SearchIcon, UserIcon, CarretIcon } from "../icons";
+import { ChevronLeftIcon, ChevronRightIcon, PlayIcon, SearchIcon, UserIcon, CarretIcon, CancelIcon } from "../icons";
 
 interface PageHeaderProps {
     variant: 'home' | 'search' | 'library' | 'playlist',
@@ -8,13 +8,13 @@ interface PageHeaderProps {
 
 const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
     return ( 
-        <header className="flex items-center space-x-4 py-4 px-4 sm:px-8 h-16 w-full sticky z-30 top-0 left-0 bg-sdark-base">
+        <header className="@container/header flex items-center space-x-4 py-4 px-4 sm:px-8 h-16 w-full sticky z-30 top-0 left-0 bg-transparent">
             {/* Navigation */}
             <div className={`${(!isLoggedIn || variant === 'home') && 'grow'} flex space-x-4`}>
                 <button className="flex h-8 w-8 items-center justify-center bg-black rounded-full">
                     <ChevronLeftIcon width={16} height={16} />
                 </button>
-                <button className="hidden md:flex h-8 w-8 items-center justify-center bg-black rounded-full disabled:opacity-30" disabled>
+                <button className="hidden @cmd/header:flex h-8 w-8 items-center justify-center bg-black rounded-full disabled:opacity-30" disabled>
                     <ChevronRightIcon width={16} height={16} />
                 </button>
             </div>
@@ -62,20 +62,21 @@ const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
             {/* Search Form */}
             {variant === 'search' && (
                 <div className={`${true && 'grow'}`}>
-                    <form action="" className="relative h-10 w-full md:w-96 flex items-center">
+                    <form action="" className="relative h-10 w-full @cmd/header:w-96 flex items-center">
+                        <SearchIcon width={24} height={24} color="black" className="absolute left-3" />
                         <input 
                             type="text" 
                             placeholder="What do you want to listen to?"
-                            className="rounded-full h-full w-full pl-12 text-sm bg-white focus-visible:outline-none" 
+                            className="rounded-full h-full w-full px-12 text-sm bg-white focus-visible:outline-none" 
                         />
-                        <SearchIcon width={24} height={24} color="black" className="absolute left-3" />
+                        <CancelIcon width={24} height={24} color="black" className="absolute left-3" />
                     </form>
                 </div>
             )}
 
             {/* Upgrade Button */}
             {(isLoggedIn && variant === 'home' || variant === 'playlist') && (
-                <div className="hidden md:block">
+                <div className="hidden @cxs/header:block">
                     <button className="upgrade-btn">
                         <p>Upgrade</p>
                     </button>
@@ -85,14 +86,14 @@ const PageHeader: FC<PageHeaderProps> = ({ variant, isLoggedIn }) => {
             {/* Profile Option or Authentication Option */}
             {isLoggedIn ? (
                 <div>
-                    <button className="flex items-center space-x-2 text-white text-sm font-bold rounded-full bg-black p-[2px] md:pr-2">
+                    <button className="flex items-center space-x-2 text-white text-sm font-bold rounded-full bg-black p-[2px] @csm/header:pr-2">
                         <div className="flex items-center h-7 w-7 justify-center rounded-full bg-sdark-53">
                             <UserIcon width={16} height={16} />
                         </div>
-                        <p className="hidden md:block">
+                        <p className="hidden @csm/header:block">
                             Babblebey
                         </p>
-                        <CarretIcon width={16} height={16} className="hidden md:block" />
+                        <CarretIcon width={16} height={16} className="hidden @csm/header:block" />
                     </button>
                 </div>
             ) : (
