8c194518f078c4c036d0be8fd5cfe836daa205fb
babblebey <babblebey@gmail.com>
2023-02-03 18:42:42 +0100

    Corrected TimerIcon svg viewBox

--- icons/TimerIcon.tsx
+++ icons/TimerIcon.tsx
@@ -11,7 +11,7 @@ interface Props {
 const TimerIcon: FC<Props> = ({ height, width, color, className, active }) => {
     return ( 
         <>
-            <svg role="img" fill={color} height={height} width={width} aria-hidden="true" className={className} viewBox="0 0 24 24" data-encore-id="icon">
+            <svg role="img" fill={color} height={height} width={width} aria-hidden="true" className={className} viewBox="0 0 16 16" data-encore-id="icon">
                 <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z"></path>
                 <path d="M8 3.25a.75.75 0 01.75.75v3.25H11a.75.75 0 010 1.5H7.25V4A.75.75 0 018 3.25z"></path>
             </svg>
