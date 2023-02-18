04d2485f92bb9e136e62bed096925353a021886f
babblebey <babblebey@gmail.com>
2023-02-18 19:12:27 +0100

    Added new props to interface and element to component

--- components/PlaylistSection.tsx
+++ components/PlaylistSection.tsx
@@ -1,23 +1,32 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import PlaylistCard from "./PlaylistCard";
 
 interface PlaylistSectionProps {
     title: string,
-    items?: object[],
+    subtitle?: string,
+    items?: any[],
     hideOverflow?: boolean,
-    showAll?: boolean
+    showAll?: boolean,
+    withPlayBtn?: boolean
 }
  
-const PlaylistSection: FC<PlaylistSectionProps> = ({ title, items, hideOverflow, showAll }) => {
-    const [list, setList] = useState([1,2,3,4,5,6,7,8,9]);
+const PlaylistSection: FC<PlaylistSectionProps> = ({ title, subtitle, items, hideOverflow, showAll, withPlayBtn }) => {
+    const list = [1,2,3,4,5,6,7,8,9];
 
     return ( 
         <div className="space-y-5 section @container/section">
             {/* Title Section */}
             <div className="flex items-center justify-between">
-                <a href="" className="_title hover:underline">
-                    { title }
-                </a>
+                <div>
+                    <a href="" className="block _title hover:underline">
+                        { title }
+                    </a>
+                    {subtitle && (
+                        <span className="text-sm text-swhite-subdued">
+                            { subtitle }
+                        </span>
+                    )}
+                </div>
                 {showAll && (
                     <a href="" className="_link">
                         Show All
@@ -28,7 +37,7 @@ const PlaylistSection: FC<PlaylistSectionProps> = ({ title, items, hideOverflow,
             {/* Playlist Items Section */}
             <div className={`items @container/section-items ${!hideOverflow && 'gap-y-6'}`}>
                 { list.map((i, l) => (
-                    <PlaylistCard key={i} container={hideOverflow} />
+                    <PlaylistCard key={i} container={hideOverflow} withPlayBtn={withPlayBtn} />
                 )) }
             </div>
         </div>
