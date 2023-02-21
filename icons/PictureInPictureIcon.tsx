38493d67dbded206636fd958ab27760a22c8f923
babblebey <babblebey@gmail.com>
2023-02-21 18:21:55 +0100

    Corrected Icon svg fillRule

--- icons/PictureInPictureIcon.tsx
+++ icons/PictureInPictureIcon.tsx
@@ -11,8 +11,8 @@ const PictureInPictureIcon: FC<Props> = ({ height, width, color, className }) =>
     return ( 
         <>
             <svg role="img" fill={color} height={height} width={width} aria-hidden="true" className={className} viewBox="0 0 16 16" data-encore-id="icon">
-                <g fill={color} fill-rule="evenodd" className={className}>
-                    <path d="M1 3v9h14V3H1zm0-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z" fill-rule="nonzero"></path>
+                <g fill={color} fillRule="evenodd" className={className}>
+                    <path d="M1 3v9h14V3H1zm0-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z" fillRule="nonzero"></path>
                     <path d="M10 8h4v3h-4z"></path>
                 </g>            
             </svg>
