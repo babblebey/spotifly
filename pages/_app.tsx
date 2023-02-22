eee0004263a9fe8a158de21fbd0895313dffae64
babblebey <babblebey@gmail.com>
2023-02-22 13:35:30 +0100

    Added new layout specific for '/auth/signin' page/route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,43 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { circular } from "../fonts";
 import Navbar from "../components/Navbar";
-import Player from "../components/Player";
+import Player from "../components/Player"; 
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <main className={`${circular.variable} font-sans`}>
-      <div className="sm:flex w-full sm:h-screen sm:overflow-hidden">
-        {/* Navbar */}
-        <Navbar isLoggedIn />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter();
 
-        {/* Pages Content */}
-        <div className="page relative bg-sdark-base">
-          <Component {...pageProps} />
+  // Layout Specifically for Sign In Page
+  if (router.pathname === '/auth/signin') {
+    return (
+      <SessionProvider session={session}>
+        <div className={`${circular.variable} font-sans`}>
+          <Component { ...pageProps } />
         </div>
+      </SessionProvider>
+    )
+  }
 
-        {/* Player */}
-        <Player />
+  // Layout for all other Pages
+  return (
+    <SessionProvider session={session}>
+      <div className={`${circular.variable} font-sans`}>
+        <div className="sm:flex w-full sm:h-screen sm:overflow-hidden">
+          {/* Navbar */}
+          <Navbar />
+
+          {/* Pages Content */}
+          <div className="page relative bg-sdark-base">
+            <Component {...pageProps} />
+          </div>
+
+          {/* Player */}
+          <Player />
+        </div>
       </div>
-    </main>
+    </SessionProvider>
   )
 }
 
