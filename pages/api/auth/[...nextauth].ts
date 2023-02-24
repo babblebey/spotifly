b2c370c11fe26daa87080713d4c05b44f6f9060d
babblebey <babblebey@gmail.com>
2023-02-24 23:49:44 +0100

    refactored

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,39 +1,37 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
 async function refreshAccessToken(token) {
     try {
-        const url = "https://accounts.spotify.com/api/token?" +
-            new URLSearchParams({
+        const url = "https://accounts.spotify.com/api/token?" + new URLSearchParams({
                 client_id: process.env.SPOTIFY_CLIENT_ID as string,
                 client_secret: process.env.SPOTIFY_CLIENT_SECRET as string,
                 grant_type: "refresh_token",
                 refresh_token: token.refreshToken,
             });
-
+        
         const response = await fetch(url, {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
             },
             method: "POST",
         });
-
+    
         const refreshedTokens = await response.json();
-
+    
         if (!response.ok) {
             throw refreshedTokens;
         }
-
+    
         return {
             ...token,
             accessToken: refreshedTokens.access_token,
             accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
             refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
         };
-
     } catch (error) {
         console.log(error);
-
+    
         return {
             ...token,
             error: "RefreshAccessTokenError",
@@ -41,7 +39,7 @@ async function refreshAccessToken(token) {
     }
 }
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
     providers: [
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID as string,
@@ -52,10 +50,10 @@ export const authOptions = {
                     scope: "user-read-email,user-modify-playback-state,playlist-read-private,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read"
                 }
             }
-        })
+        }),
     ],
 
-    callback: {
+    callbacks: {
         async jwt({ token, user, account }) {
             // Initial sign in
             if (account && user) {
@@ -66,22 +64,22 @@ export const authOptions = {
                     user,
                 };
             }
-    
+
             // Return previous token if the access token has not expired yet
             if (Date.now() < token.accessTokenExpires) {
                 return token;
             }
-    
+
             // Access token has expired, try to update it
             return refreshAccessToken(token);
         },
         async session({ session, token }) {
             session.user = token.user;
             session.accessToken = token.accessToken;
-            session.error = token.error;
-    
-            return session;
-        },
+            session.error = token.error
+
+            return session
+        }
     },
     
     pages: {
