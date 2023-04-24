8ef379a3ebbb6ce5f72152432a328a75a5f86a51
babblebey <babblebey@gmail.com>
2023-04-24 21:39:21 +0100

    Refactored component

--- pages/collection/albums.tsx
+++ pages/collection/albums.tsx
@@ -1,15 +1,21 @@
-import { NextPage } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
+import { getToken } from "next-auth/jwt";
 import Head from "next/head";
 import PageFooter from "../../components/PageFooter";
 import PageHeader from "../../components/PageHeader";
+import PlaylistCard from "../../components/PlaylistCard";
 import PlaylistSection from "../../components/PlaylistSection";
 
+import sampleData from "../../data/userAlbumsData.json"
+import { GetUsersSavedAlbumsResponse } from "../../types/spotify-api";
+
 interface AlbumsProps {
-    items: object[]
+    
 }
 
-const Albums: NextPage<AlbumsProps> = ({ items }) => {
-    const list = [1,2,3,4,5,6,7,8,9];
+const Albums: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    console.log(data)
+    const { items } = data as GetUsersSavedAlbumsResponse
 
     return (
         <div className="relative">
@@ -17,10 +23,20 @@ const Albums: NextPage<AlbumsProps> = ({ items }) => {
                 <title>Spotifly - Your Library</title>
             </Head>
 
-            <PageHeader variant="library" className="bg-transparent" />
+            <PageHeader variant="library" backgroundColor="#070707" />
 
-            <main className="content @container">
-                <PlaylistSection title="Albums" />
+            <main className="content section @container space-y-5">
+                <div className="flex items-center">
+                    <h2 className="_title">
+                        Albums
+                    </h2>
+                </div>
+
+                <div className="items @container/section-items gap-y-6">
+                    { items.map((item, i) => (
+                        <PlaylistCard key={i} item={ item.album } withPlayBtn />
+                    )) }
+                </div>
             </main>
 
             <PageFooter />
@@ -29,3 +45,16 @@ const Albums: NextPage<AlbumsProps> = ({ items }) => {
 }   
 
 export default Albums
+
+export const getServerSideProps: GetServerSideProps = async ({ req }: GetServerSidePropsContext) => {
+    const secret = process.env.NEXTAUTH_SECRET;
+    const token = await getToken({ req, secret });
+    const headers = { 'Authorization': 'Bearer ' + token?.accessToken }
+
+    // const response = await fetch(`https://api.spotify.com/v1/me/albums`, { headers });
+    // const data = await response.json();
+
+    return {
+        props: { data: sampleData }
+    }
+}
